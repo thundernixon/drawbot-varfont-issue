211bd5873d05b37594e519afbf7385fe8145614f
Stephen Nixon <swr410@gmail.com>
2018-10-09 14:26:08 -0400

    add other fonts, update script

--- static_vf_comparison-drawbot-100818.py
+++ static_vf_comparison-drawbot-100818.py
@@ -1,21 +1,27 @@
-rect(0,0,1000,1000)
+W,H = 860, 340
 
-testString = "Lorem \nTestem \n845"
-setLineHeight = 260
-setFontSize = 250
-xPos, yPos = 80, 664
+newPage(W,H)
+
+rect(0,0,W,H)
+
+testString = "In statistical modeling, regression analysis is a set of statistical processes for estimating the relationships among variables. It includes many techniques for modeling and analyzing several variables, when the focus is on the relationship between a dependent variable and one or more independent variables (or 'predictors'). More specifically, regression analysis helps one understand how the typical value of the dependent variable (or 'criterion variable') changes when any one of the independent variables is varied, while the other independent variables are held fixed. \n \nMost commonly, regression analysis estimates the conditional expectation of the dependent variable given the independent variables – that is, the average value of the dependent variable when the independent variables are fixed. Less commonly, the focus is on a quantile, or other location parameter of the conditional distribution of the dependent variable given the independent variables. In all cases, a function of the independent variables called the regression function is to be estimated. In regression analysis, it is also of interest to characterize the variation of the dependent variable around the prediction of the regression function using a probability distribution. A related but distinct approach is Necessary Condition Analysis[1] (NCA), which estimates the maximum (rather than average) value of the dependent variable for a given value of the independent variable (ceiling line rather than central line) in order to identify what value of the independent variable is necessary but not sufficient for a given value of the dependent variable."
+setLineHeight = 18
+setFontSize = 16
+# xPos, yPos = 80, 664
+xPos, yPos = W/20,H/20 
+txtW, txtH =  W-(W/10),H-(H/10)
 
 lineHeight(setLineHeight)
 
 # linking a specific font file for testing
-font("EncodeSans-Regular.ttf", setFontSize)
+font("./EncodeSans-Regular.ttf", setFontSize)
 
 fill(1,0,1)
 
-text(testString, (xPos, yPos))
+textBox(testString, (xPos, yPos, txtW, txtH))
 
 # Linking a specific font file for testing. The text() method fails for this font, even though the font works as expected in FontView and on Axis-Praxis.org
-font("EncodeSans-mathfix-normal_wdth-VF.ttf", setFontSize)
+font("./EncodeSans-simplefix-normal_wdth-VF.ttf", setFontSize)
 
 # # It *does* work if I link to another variable font, such as Avenir Next Variable.
 # font("AvenirNext_Variable.ttf", setFontSize)
@@ -28,7 +34,8 @@ for axis, data in listFontVariations().items():
 fontVariations(wght=400.0,wdth=500.0)
 
 blendMode("screen")
-fill(0,.75,1)
+fill(0,1,0)
 
 # I recieve a NSInvalidArgumentException here, which Apple describes as "Name of an exception that occurs when you pass an invalid argument to a method, such as a nil pointer where a non-nil object is required." However, I only receive this error when I set the font variations of the above variable font. So, the arguments should be valid.
-text(testString, (xPos, yPos))
+# text(testString, (xPos, yPos))
+textBox(testString, (xPos, yPos, txtW, txtH))
